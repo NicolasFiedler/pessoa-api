815ac011480e2406d179ebc14cfb22dcbb1af297
NicolasFiedler <nicolasxfiedler@gmail.com>
2022-03-17 22:54:08 -0300

    upload pessoa-api for heroku

---
+++ src/main/java/br/com/dbc/vemser/pessoaapi/service/PessoaService.java
@@ -0,0 +1,68 @@
+package br.com.dbc.vemser.pessoaapi.service;
+
+import br.com.dbc.vemser.pessoaapi.dto.pessoa.PessoaCreateDTO;
+import br.com.dbc.vemser.pessoaapi.dto.pessoa.PessoaDTO;
+import br.com.dbc.vemser.pessoaapi.entity.Pessoa;
+import br.com.dbc.vemser.pessoaapi.exception.RegraDeNegocioException;
+import br.com.dbc.vemser.pessoaapi.repository.PessoaRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PessoaService {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private EmailService emailService;
+
+//    public PessoaService(){
+//        pessoaRepository = new PessoaRepository();
+//    }
+
+    public PessoaDTO create(PessoaCreateDTO pessoaCreateDTO){
+
+        Pessoa p = objectMapper.convertValue(pessoaCreateDTO, Pessoa.class);
+        p = pessoaRepository.create(p);
+        if (p != null){
+            emailService.sendEmail(objectMapper.convertValue(p, PessoaDTO.class), "Novo Cadastro","email-template-cria-pessoa.ftl");
+        }
+        return objectMapper.convertValue(p, PessoaDTO.class);
+    }
+
+    public List<PessoaDTO> list(){
+        return pessoaRepository.list().stream()
+                .map(pessoa -> objectMapper.convertValue(pessoa, PessoaDTO.class))
+                .toList();
+    }
+
+    public PessoaDTO update(Integer id, PessoaCreateDTO pessoaAtualizar) throws Exception {
+        Pessoa p = pessoaRepository.update(id, objectMapper.convertValue(pessoaAtualizar, Pessoa.class));
+        return objectMapper.convertValue(p, PessoaDTO.class);
+    }
+
+    public PessoaDTO delete(Integer id) throws Exception {
+        return objectMapper.convertValue(pessoaRepository.delete(id), PessoaDTO.class);
+    }
+
+    public List<PessoaDTO> listByName(String nome) {
+        return pessoaRepository.listByName(nome).stream()
+                .map(pessoa -> objectMapper.convertValue(pessoa, PessoaDTO.class))
+                .toList();
+    }
+
+    public PessoaDTO getPessoaById (Integer id) throws Exception {
+       return pessoaRepository.list().stream()
+                .filter(pessoa -> pessoa.getIdPessoa().equals(id))
+                .findFirst()
+                .map(pessoa -> objectMapper.convertValue(pessoa, PessoaDTO.class))
+                .orElseThrow(() -> new RegraDeNegocioException("Pessa nao encontrada"));
+    }
+}
